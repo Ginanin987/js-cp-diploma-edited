4a2763ce3be6da3fb8436be364a713d7eb7b1c2d
Victoria Zhuravleva <Zhuravleva987@mail.ru>
2023-09-05 19:36:54 +0400

    hall.js place selection

--- sources/client/hall.js
+++ sources/client/hall.js
@@ -1,4 +1,6 @@
 console.log(localStorage)
+const rows = Array.from(document.querySelectorAll(".conf-step__row"))
+let rowsObject = {}
 
 document.querySelector(".buying__info-title").textContent =
   localStorage.getItem("filmName")
@@ -46,26 +48,46 @@ Array.from(
     } else {
       element.classList.add("conf-step__chair_selected")
     }
+
+    // getRowChair()
   }
 })
 
-document
-  .querySelector(".acceptin-button")
-  .addEventListener("click", (event) => {
-    Array.from(
-      document
-        .querySelector(".conf-step__wrapper")
-        .querySelectorAll(".conf-step__chair_selected")
-    ).forEach((element) => {
-      element.classList.remove("conf-step__chair_selected")
-      element.classList.add("conf-step__chair_taken")
-    })
+document.querySelector(".acceptin-button").addEventListener("click", () => {
+  localStorage.selectedPlaces = getRowChair().join(", ")
 
-    //     //     // fetch("https://jscp-diplom.netoserver.ru/", {
-    //     //     // method: "POST",
-    //     //     // body: "event=sale_add&timestamp=${value1}&hallId=${value2}&seanceId=${value3}&hallConfiguration=${value4}",
-    //     //     // headers: {
-    //     //     //   "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-    //     //     // }
-    //     //   })
-  })
+  //     //     // fetch("https://jscp-diplom.netoserver.ru/", {
+  //     //     // method: "POST",
+  //     //     // body: "event=sale_add&timestamp=${value1}&hallId=${value2}&seanceId=${value3}&hallConfiguration=${value4}",
+  //     //     // headers: {
+  //     //     //   "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+  //     //     // }
+  //     //   })
+})
+
+function getRowChair() {
+  for (let i = 1; i < rows.length + 1; i++) {
+    rowsObject[i] = Array.from(
+      rows[i - 1].querySelectorAll(".conf-step__chair")
+    )
+  }
+
+  for (let row in rowsObject) {
+    rowsObject[row] = rowsObject[row].filter(
+      (element) => !element.classList.contains("conf-step__chair_disabled")
+    )
+  }
+
+  let selectedPlaces = []
+
+  for (let row in rowsObject) {
+    rowsObject[row].forEach((element) => {
+      if (element.classList.contains("conf-step__chair_selected")) {
+        selectedPlaces.push(`${row}/${rowsObject[row].indexOf(element) + 1}`)
+        element.classList.remove("conf-step__chair_selected")
+        element.classList.add("conf-step__chair_taken")
+      }
+    })
+  }
+  return selectedPlaces
+}
